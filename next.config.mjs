6bfeabc0d3df37e53b66a31ded0b00e287290249
Yurii Kozlov <yurka22870@gmail.com>
2024-09-25 13:22:47 +0300

    feat: set up base configuration

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  sassOptions: {
+    additionalData: `@import "src/styles/utils/variables.scss";`,
+  },
+}
 
 export default nextConfig;
